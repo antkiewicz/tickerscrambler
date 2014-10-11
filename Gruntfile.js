698d2bb8b2360a355641efbd01264101e5368fe3
Lukasz Antkiewicz <dev@lantkiewicz.com>
2014-10-11 15:14:36 -0400

    Add watch task.

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,12 +13,17 @@ module.exports = function(grunt) {
 				src: '<%= pkg.name %>.js',
 				dest: '<%= pkg.name %>.min.js'
 			}
+		},
+		watch: {
+			files: ['<%= pkg.name %>.js'],
+			tasks: ['jshint']
 		}
 	});
 
 	// Load the plugin that provides the "uglify" task.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task.
 	grunt.registerTask('default', ['jshint', 'uglify']);
